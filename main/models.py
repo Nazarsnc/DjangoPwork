d4aaf1af7a207f32873233d3bb454b3877f2bedb
Nazarsnc <143934973+Nazarsnc@users.noreply.github.com>
2025-05-30 16:08:57 +0300

    models.py

--- main/models.py
+++ main/models.py
@@ -1,32 +1,31 @@
 from django.db import models
 
 class Poll(models.Model):
-    ТЕМИ_ОПИТУВАНЬ = [
-        ('суспільство', 'Суспільство'),
-        ('технології', 'Технології'),
-        ('освіта', 'Освіта'),
-        ('політика', 'Політика'),
-        ('інше', 'Інше'),
+    POLL_TOPICS = [
+        ('society', 'Суспільство'),
+        ('technology', 'Технології'),
+        ('education', 'Освіта'),
+        ('politics', 'Політика'),
+        ('other', 'Інше'),
     ]
 
-    назва = models.CharField("Назва опитування", max_length=200)
-    опис = models.TextField("Опис опитування", blank=True)
-    дата_створення = models.DateField("Дата створення", auto_now_add=True)
-    активне = models.BooleanField("Активне?", default=True)
-    категорія = models.CharField("Категорія", max_length=50, choices=ТЕМИ_ОПИТУВАНЬ, default='інше')
-    дозволено_анонімно = models.BooleanField("Анонімне голосування?", default=True)
-    мін_вік = models.PositiveIntegerField("Мінімальний вік", default=18)
-    кількість_голосів = models.PositiveIntegerField("Кількість голосів", default=0)
+    title = models.CharField("Назва опитування", max_length=200)
+    description = models.TextField("Опис опитування", blank=True)
+    created_date = models.DateField("Дата створення", auto_now_add=True)
+    is_active = models.BooleanField("Активне?", default=True)
+    category = models.CharField("Категорія", max_length=50, choices=POLL_TOPICS, default='other')
+    allow_anonymous = models.BooleanField("Анонімне голосування?", default=True)
+    min_age = models.PositiveIntegerField("Мінімальний вік", default=18)
+    votes_count = models.PositiveIntegerField("Кількість голосів", default=0)
 
     def __str__(self):
-        return self.назва
+        return self.title
 
 
 class Vote(models.Model):
-    опитування = models.ForeignKey(Poll, on_delete=models.CASCADE, verbose_name="Опитування")
-    варіант = models.CharField("Вибраний варіант", max_length=100)
-    дата_голосування = models.DateTimeField("Час голосування", auto_now_add=True)
-
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, verbose_name="Опитування")
+    choice = models.CharField("Вибраний варіант", max_length=100)
+    voted_at = models.DateTimeField("Час голосування", auto_now_add=True)
 
     def __str__(self):
-        return f"Голос за: {self.варіант}"
+        return f"Голос за: {self.choice}"
